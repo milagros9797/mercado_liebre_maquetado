440c034fc6243a9ec179e43b478b021dae0e786d
Milagros <miilyailen97@gmail.com>
2023-11-12 17:38:02 -0300

    agrego formulario de registro y login  en HTML

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = 3030;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
 
-app.use(express.static(path.join(__dirname,'public')))
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
-
-
-
-app.listen(PORT,console.log(`Server running in http://localhost:${PORT}`))
+app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`));
